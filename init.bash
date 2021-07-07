352567d015c1c818135cd7f0d64005f7f92702d9
Bhavi Dhingra <bhavi.dhingra1@swiggy.in>
2021-07-08 00:17:49 +0530

    update init scripts according to release-1.0

--- init.bash
+++ init.bash
@@ -2,21 +2,18 @@
 # Setup custom-git
 # ----------------
 
-export FZF_DEFAULT_OPTS="--reverse --border --height 80%"
-
 export CUSTOM_GIT_HOME="${HOME}"/.custom-git
-export CUSTOM_CONSOLE_HOME="${CUSTOM_GIT_HOME}"/custom-console
+export CUSTOM_CONSOLE_HOME="${CUSTOM_GIT_HOME}"/custom-console-bash
 
-if [[ ! "${PATH}" == *"${CUSTOM_GIT_HOME}/cmd"* ]]; then
-    export PATH="${PATH:+${PATH}:}${CUSTOM_GIT_HOME}/cmd"
+export __CUSTOM_GIT_UTIL="$CUSTOM_GIT_HOME"/util
+export __CUSTOM_CONSOLE_UTIL="$CUSTOM_CONSOLE_HOME"/util
+
+if [[ ! "$PATH" == *${CUSTOM_CONSOLE_HOME}/cmd* ]]; then
+    export PATH="${CUSTOM_CONSOLE_HOME}/cmd${PATH:+:${PATH}}"
 fi
-if [[ ! "${PATH}" == *"${CUSTOM_GIT_HOME}/util"* ]]; then
-    export PATH="${PATH:+${PATH}:}${CUSTOM_GIT_HOME}/util"
-fi
-if [[ ! "${PATH}" == *"${CUSTOM_CONSOLE_HOME}/cmd"* ]]; then
-    export PATH="${PATH:+${PATH}:}${CUSTOM_CONSOLE_HOME}/cmd"
-fi
-if [[ ! "${PATH}" == *"${CUSTOM_CONSOLE_HOME}/util"* ]]; then
-    export PATH="${PATH:+${PATH}:}${CUSTOM_CONSOLE_HOME}/util"
+if [[ ! "$PATH" == *${CUSTOM_GIT_HOME}/cmd* ]]; then
+    export PATH="${CUSTOM_GIT_HOME}/cmd${PATH:+:${PATH}}"
 fi
 
+export FZF_DEFAULT_OPTS="--reverse --border --ansi --preview-window wrap:down:70% --tabstop=4"
+export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --color='preview-fg:-1'"

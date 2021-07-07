dcbae4a9eb50ff85ca8bd967e4f00f960c9a5cda
Bhavi Dhingra <bhavi.dhingra1@swiggy.in>
2021-07-08 00:33:20 +0530

    append custom-git cmd path to PATH env variable

--- init.zsh
+++ init.zsh
@@ -8,11 +8,11 @@ export CUSTOM_CONSOLE_HOME="${CUSTOM_GIT_HOME}"/custom-console-bash
 export __CUSTOM_GIT_UTIL="$CUSTOM_GIT_HOME"/util
 export __CUSTOM_CONSOLE_UTIL="$CUSTOM_CONSOLE_HOME"/util
 
-if [[ ! "$PATH" == *${CUSTOM_CONSOLE_HOME}/cmd* ]]; then
-    export PATH="${CUSTOM_CONSOLE_HOME}/cmd${PATH:+:${PATH}}"
-fi
 if [[ ! "$PATH" == *${CUSTOM_GIT_HOME}/cmd* ]]; then
-    export PATH="${CUSTOM_GIT_HOME}/cmd${PATH:+:${PATH}}"
+    export PATH="${PATH:+${PATH}:}${CUSTOM_GIT_HOME}/cmd"
+fi
+if [[ ! "$PATH" == *${CUSTOM_CONSOLE_HOME}/cmd* ]]; then
+    export PATH="${PATH:+${PATH}:}${CUSTOM_CONSOLE_HOME}/cmd"
 fi
 
 export FZF_DEFAULT_OPTS="--reverse --border --ansi --preview-window wrap:down:70% --tabstop=4"

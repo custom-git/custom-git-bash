719cfe7bfc7ddea660e26e2cef50c44cbc3c85ef
Bhavi Dhingra <bhavi.dhingra1@swiggy.in>
2021-07-29 19:51:47 +0530

    build(init): use init.zsh instead custom-git.zsh

---
+++ init.bash
@@ -0,0 +1,19 @@
+# ----------------
+# Setup custom-git
+# ----------------
+
+export CUSTOM_GIT_HOME="${HOME}"/.custom-git
+export CUSTOM_CONSOLE_HOME="${CUSTOM_GIT_HOME}"/custom-console-bash
+
+export __CUSTOM_GIT_UTIL="$CUSTOM_GIT_HOME"/util
+export __CUSTOM_CONSOLE_UTIL="$CUSTOM_CONSOLE_HOME"/util
+
+if [[ ! "$PATH" == *${CUSTOM_GIT_HOME}/cmd* ]]; then
+    export PATH="${PATH:+${PATH}:}${CUSTOM_GIT_HOME}/cmd"
+fi
+if [[ ! "$PATH" == *${CUSTOM_CONSOLE_HOME}/cmd* ]]; then
+    export PATH="${PATH:+${PATH}:}${CUSTOM_CONSOLE_HOME}/cmd"
+fi
+
+export FZF_DEFAULT_OPTS="--reverse --border --ansi --preview-window wrap:down:70% --tabstop=4"
+export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --color='preview-fg:-1'"
